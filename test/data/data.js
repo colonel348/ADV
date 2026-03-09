99d2e252cafea1d2a66c9ad8ddb96df833ac2a1e
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-03-09 22:57:46 +0900

    1

--- test/data/data.js
+++ test/data/data.js
@@ -1,5 +1,9 @@
 const evtData = [
 
+  { evtId: "AK-02", evtNm: "特訓編", cpt:[
+    { cptId: "1", ptnId: "1", plcNm: "スタジオ"}
+  ]},
+
   { evtId: "SA-01", evtNm: "決戦編", cpt:[
     { cptId: "2", ptnId: "1", plcNm: "ビビッドストリート"},
     { cptId: "3", ptnId: "2", plcNm: "ビビッドストリート"}
