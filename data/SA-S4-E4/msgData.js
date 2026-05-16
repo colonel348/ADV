9a48dad0d007c8a7aa125d7ca003f4cb5cdf742b
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-05-16 14:53:24 +0900

    1

---
+++ data/SA-S4-E4/msgData.js
@@ -0,0 +1,21 @@
+window.msgData = [
+
+/*************************************************
+ * CPT-1
+ *************************************************/
+
+{ cptId: "1", msgInfo:[
+
+  { movId: "plg", title: "ダンススタジオ"},
+    { tmgId:"Z", chrNm: "杏", msgTxt: "test"},
+
+  { movId: "evt1"},
+    { tmgId:"A", chrNm: "杏", msgTxt: "test1"},
+    { tmgId:"L", chrNm: "冬弥", msgTxt: "test2"},
+
+  { movId: "elg"},
+    { tmgId:"Z", chrNm: "杏", msgTxt: "test"}
+
+]}
+
+];
