9674f115c517482ac7a1609c2ab4fc225c0120e5
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-05-16 13:26:08 +0900

    1

--- js/event.js
+++ js/event.js
@@ -42,8 +42,7 @@ let currentSrcL = "";
 // A動画終了何秒前に次を開始するか
 const ACTION_SWITCH_BEFORE = 0.33;
 // L動画終了何秒前に次を開始するか
-const LOOP_SWITCH_BEFORE = 0.30
-;
+const LOOP_SWITCH_BEFORE = 0.32;
 // 次L動画play後
 // fade開始まで待つms
 const LOOP_FADE_WAIT = 260;
