f515316e5aaed89ffc86e6681d0fdd357aec56ff
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-05-24 23:08:56 +0900

    1

--- data/evtData.js
+++ data/evtData.js
@@ -8,38 +8,58 @@ const evtData = [
     { cptId: "2", extLv: "C"},
   ]},
 
+//  { evtId: "FF-S2-C2", evtNm: "慰め編", cpt:[
+// ▲    { cptId: "1", extLv: "B"},
+// ▲    { cptId: "2", extLv: "C"},
+//  ]},
+
 /*************************************************
  * 小豆沢こはね
  *************************************************/
 
   { evtId: "AK-S2-C1", evtNm: "特訓編", cpt:[
     { cptId: "1", extLv: "B"},
-    { cptId: "2", extLv: "C"}
+// ▲   { cptId: "2", extLv: "C"}
   ]},
 
-  { evtId: "AK-S2-C2", evtNm: "キャンバスに熱を落として", cpt:[
+  { evtId: "AK-S2-C2", evtNm: "デッサン編", cpt:[
     { cptId: "2", extLv: "B"},
     { cptId: "3", extLv: "C"}
   ]},
 
-  { evtId: "AK-S3-E1", evtNm: "孤独な朝のストリート", cpt:[
+  { evtId: "AK-S3-E1", evtNm: "登校編", cpt:[
     { cptId: "1", extLv: "B"},
     { cptId: "2", extLv: "E"}
   ]},
+
+//  { evtId: "AK-S4-E1", evtNm: "アメリカ編", cpt:[
+// ▲   { cptId: "1", extLv: "B"},
+// ▲   { cptId: "2", extLv: "E"},
+// ▲   { cptId: "3", extLv: "E"}
+//  ]},
 
 /*************************************************
  * 白石杏
  *************************************************/
 
+//  { evtId: "SA-S1-B1", evtNm: "挑発編", cpt:[
+// ▲   { cptId: "1", extLv: "B", ptnId: "V3", plcNm: "体育倉庫"},
+// ▲   { cptId: "2", extLv: "B", ptnId: "L4", plcNm: "体育倉庫"}
+//  ]},
+
   { evtId: "SA-S2-C1", evtNm: "練習編", cpt:[
-    { cptId: "1", extLv: "C"},
+//    { cptId: "1", extLv: "C"},
     { cptId: "2", extLv: "C"}
   ]},
-  
+
+//  { evtId: "SA-S2-D1", evtNm: "補習編", cpt:[
+// ▲   { cptId: "1", extLv: "A", ptnId: "L2", plcNm: "教室"},
+// ▲   { cptId: "2", extLv: "D", ptnId: "V3", plcNm: "教室"}
+//  ]},
 
   { evtId: "SA-S4-E1", evtNm: "決戦編", cpt:[
-    { cptId: "2", extLv: "E", },
-    { cptId: "3", extLv: "E", }
+// ▲   { cptId: "2", extLv: "E"},
+    { cptId: "3", extLv: "E"}
   ]}
 
 ];
