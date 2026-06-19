0d86a02e8e41d5f8264853bd5c9cb82ae041fb8c
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-06-19 18:10:20 +0900

    1

--- data/evtData.js
+++ data/evtData.js
@@ -5,15 +5,15 @@ const evtData = [
  *************************************************/
 
   { evtId: "FF-DS-B1", evtNm: "内緒の記念撮影", cpt:[
-    { cptId: "1", cmpRt: "●"},
+    { cptId: "1", cmpRt: "×", memo:""},
   ]},
 
   { evtId: "FF-LV-B1", evtNm: "背伸びの誘惑", cpt:[
-    { cptId: "1", cmpRt: "D"},
+    { cptId: "1", cmpRt: "×", memo:""},
   ]},
 
   { evtId: "FF-NR-A1", evtNm: "dummy", cpt:[
-    { cptId: "1", cmpRt: "B"}
+    { cptId: "1", cmpRt: "", memo:""}
   ]},
 
 /*************************************************
@@ -21,48 +21,48 @@ const evtData = [
  *************************************************/
 
   { evtId: "AK-DS-B1", evtNm: "描かれる身体", cpt:[
-    { cptId: "1", cmpRt: "B"},
-    { cptId: "2", cmpRt: "C"}
+    { cptId: "1", cmpRt: "△", memo:""},
+    { cptId: "2", cmpRt: "△", memo:""}
   ]},
 
   { evtId: "AK-DS-D1", evtNm: "恥辱の通学路", cpt:[
-    { cptId: "1", cmpRt: "B"},
-    { cptId: "2", cmpRt: "E"}
+    { cptId: "1", cmpRt: "△", memo:""},
+    { cptId: "2", cmpRt: "×", memo:""}
   ]},
 
   { evtId: "AK-LV-B1", evtNm: "dummy", cpt:[
-    { cptId: "1", cmpRt: "B"}
+    { cptId: "1", cmpRt: "", memo:""}
   ]},
 
   { evtId: "AK-NR-B1", evtNm: "快感に抗う試練", cpt:[
-    { cptId: "1", cmpRt: "●"},
-    { cptId: "2", cmpRt: "●"}
+    { cptId: "1", cmpRt: "●", memo:"調教に変更してもよい"},
+    { cptId: "2", cmpRt: "●", memo:""}
   ]},
 
   { evtId: "AK-NR-D1", evtNm: "裸で示す成長", cpt:[
-    { cptId: "1", cmpRt: "B"},
-    { cptId: "2", cmpRt: "E"},
+    { cptId: "1", cmpRt: "●", memo:""},
+    { cptId: "2", cmpRt: "●", memo:""},
   ]},
 
 /*************************************************
  * 白石杏
  *************************************************/
 
-  { evtId: "SA-DS-A1", evtNm: "強がりの限界", cpt:[
-    { cptId: "1", cmpRt: "B"}
+  { evtId: "SA-DS-A1", evtNm: "放課後に堕ちて", cpt:[
+    { cptId: "1", cmpRt: "●", memo:""}
   ]},
 
   { evtId: "SA-DS-C1", evtNm: "教室に溢れる熱", cpt:[
-    { cptId: "1", cmpRt: "D"}
+    { cptId: "1", cmpRt: "△", memo:""}
   ]},
 
   { evtId: "SA-LV-B1", evtNm: "魅了の第一歩", cpt:[
-    { cptId: "1", cmpRt: "C"}
+    { cptId: "1", cmpRt: "×", memo:""}
   ]},
 
   { evtId: "SA-NR-D1", evtNm: "裸心の絶唱", cpt:[
-    { cptId: "1", cmpRt: "E"},
-    { cptId: "2", cmpRt: "E"}
+    { cptId: "1", cmpRt: "△", memo:""},
+    { cptId: "2", cmpRt: "△", memo:""}
   ]}
 
 ];
