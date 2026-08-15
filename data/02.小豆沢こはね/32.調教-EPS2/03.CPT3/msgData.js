9d86dcf8d8b878c7d82cf3ec62d172463b1dff7e
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-08-16 00:41:58 +0900

    1

---
+++ data/02.小豆沢こはね/32.調教-EPS2/03.CPT3/msgData.js
@@ -0,0 +1,12 @@
+﻿window.msgData = [
+
+  { movId: "evt1" },
+    { msgId: "N", msgTxt: "" },
+    { msgId: "A", msgTxt: "" },
+    { msgId: "A", msgTxt: "" },
+    { msgId: "L", msgTxt: "" },
+  { movId: "evt2" },
+    { msgId: "A", msgTxt: "" },
+    { msgId: "L", msgTxt: "" },
+
+];
