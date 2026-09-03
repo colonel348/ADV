d5241b5fb81ce58eaf6d9317268d8c0524b19e54
hiroki_iwahashi <iwahashi.kfc@gmail.com>
2026-09-04 00:02:22 +0900

    1

--- data/02.小豆沢こはね/33.調教-EPS3/01.CPT1/msgData.js
+++ data/02.小豆沢こはね/33.調教-EPS3/01.CPT1/msgData.js
@@ -2,12 +2,8 @@
 
   { movId: "evt1" },
     { msgId: "N", msgTxt: "" },
-    { msgId: "L", msgTxt: "" },
-  { movId: "evt2" },
     { msgId: "A", msgTxt: "" },
-    { msgId: "A", msgTxt: "" },
-    { msgId: "A", msgTxt: "" },
-    { msgId: "L", msgTxt: "" },
+    { msgId: "L", msgTxt: "" }
 
 
 ];
